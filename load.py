c0d971fdf8b2f43c06132a1760b84b06959a550b
Jaron <jierui1998@gmail.com>
2020-05-31 01:56:34 -0500

    fixed oops

--- load.py
+++ load.py
@@ -16,7 +16,7 @@ def get_relevant_texts(trusted_sources, target_article, keyword_search=false, us
         if keyword_search:
             search_term = target_article.keywords[0]
             for i in range(1, len(target_article.keywords)):
-                search_term = search_term + " " + word
+                search_term = search_term + " " + target_article.keywords[i]
         else:
             search_term = target_article.title
         relavent_url = search(query= (search_term +" site:" + source), stop=1, pause=2)
@@ -40,7 +40,7 @@ if __name__ == '__main__':
     trusted_sources = loadsources()
     print("Enter URL for comparison")
     url = "https://www.space.com/elon-musk-emotional-spacex-astronaut-launch.html"
-    
+
     target_article = Article(url)
     target_article.download()
     target_article.parse()
