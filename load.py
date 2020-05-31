a328c00683eb1b1142b4e2c02d2ae266fce1e7a5
Jaron <jierui1998@gmail.com>
2020-05-31 01:55:21 -0500

    Updated generalization with params for use summary and keyword search

--- load.py
+++ load.py
@@ -6,30 +6,53 @@ def loadsources():
     with open(os.path.join(os.getcwd(), "trustedsources.config"), "r") as f:
         return f.read().splitlines()
 
-
-if __name__ == '__main__':
-    trusted_sources = loadsources()
-    print("Enter URL for comparison")
-    url = "https://www.space.com/elon-musk-emotional-spacex-astronaut-launch.html"
-    print(trusted_sources)
-    target_article = Article(url)
-    target_article.download()
-    target_article.parse()
-
-    target_text = target_article.text
+def get_relevant_texts(trusted_sources, target_article, keyword_search=false, use_summary=false): 
+    # assumption: article has been parse already
     other_texts = []
+    if keyword_search:
+        target_article.nlp()
 
     for source in trusted_sources:
-        relavent_url = search(query=(target_article.title + " site:" + source) , stop=1, pause=2)
+        if keyword_search:
+            search_term = target_article.keywords[0]
+            for i in range(1, len(target_article.keywords)):
+                search_term = search_term + " " + word
+        else:
+            search_term = target_article.title
+        relavent_url = search(query= (search_term +" site:" + source), stop=1, pause=2)
         for url in relavent_url:
             article = Article(url)
             break
         article.download()
         article.parse() 
-        other_texts.append(article.text)
-        print()
-        print()
-        print(article.text)
+        if use_summary:
+            article.nlp()
+            other_texts.append(article.summary)
+        else:
+            other_texts.append(article.text)
+
+
+if __name__ == '__main__':
+    keyword_search = False
+    target_summary = False
+    auxillary_summary = True
+
+    trusted_sources = loadsources()
+    print("Enter URL for comparison")
+    url = "https://www.space.com/elon-musk-emotional-spacex-astronaut-launch.html"
+    
+    target_article = Article(url)
+    target_article.download()
+    target_article.parse()
+    
+    if target_summary:
+        target_text.nlp()
+        target_text = target_article.summary
+    else:
+        target_text = target_article.text
+
+    other_texts = get_relevant_texts(trusted_sources, target_article, keyword_search=False, use_summary=auxillary_summary)
+    
     
 
 
