0452153d1b4a76841aed4a781c4277554ec186f3
Preston Lee <prestonwlee@gmail.com>
2020-06-08 00:15:45 -0700

    Revised distance func

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import nlp.tests
 import nlp.core as core
-
+import load
 if __name__ == '__main__':
+    # nlp.tests.bear_test()
     gpt2 = core.MODELS[2]
     gpt2_encoder = core.get_encoder(*gpt2)
     gpt2_distance = core.get_distance_func(gpt2_encoder)
 
-    
+
